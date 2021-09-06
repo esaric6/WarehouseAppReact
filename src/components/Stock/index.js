ebf3c8c5b95783b1fac5b2db27e8e0c6049cd695
esaric <34062977+esaric6@users.noreply.github.com>
2021-09-06 17:16:07 +0200

    Update index.js

--- src/components/Stock/index.js
+++ src/components/Stock/index.js
@@ -70,10 +70,12 @@ export default function Album(props) {
   const classes = useStyles();
 
   const [products, setProducts] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(false);
 
   const ref = firebase.firestore().collection("products");
 
+  //REALTIME GET FUNCTION
   function getProducts() {
     setLoading(true);
     ref.onSnapshot((querySnapshot) => {
@@ -88,6 +90,7 @@ export default function Album(props) {
 
   useEffect(() => {
     getProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const searchData = (pattern) => {
